29eb46c098e8c0379088e9063fd49f6a6e4492aa
Benjamin Matschke <benjaminmatschke@googlemail.com>
2023-02-28 13:31:22 +0300

    starting tests

--- db/tests.py
+++ db/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from .models import UserProfile
+from .models import Wanted
 
-# Create your tests here.
+from .models import Table
+from .models import TableData
+from .models import TableSearch
+from .models import TableCommit
+from .models import Contributor
+from .models import Tag
+from .models import Number
+from .models import NumberPAdic
+from .models import NumberComplex
+from .models import Polynomial
+
+from .models import OeisNumber
+from .models import OeisSequence
+from .models import WikipediaNumber
+
+class NumberDBTestCase(TestCase):
+    def setUp(self):
+        pass
+        
+    def test_animals_can_speak(self):
+        #lion = Animal.objects.get(name="lion")
+        #cat = Animal.objects.get(name="cat")
+        #self.assertEqual(lion.speak(), 'The lion says "roar"')
+        #self.assertEqual(cat.speak(), 'The cat says "meow"')
+        pass
